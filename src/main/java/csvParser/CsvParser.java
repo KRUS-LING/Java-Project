8395fb36f6548bda482dd6daa3cd1497cad7a1c5
8848 <egorguk2005@outlook.com>
2024-11-20 23:44:35 +0500

    Add gitignore

--- src/main/java/csvParser/CsvParser.java
+++ src/main/java/csvParser/CsvParser.java
@@ -147,17 +147,4 @@ public class CsvParser {
 
         return student;
     }
-
-    public static void main(String[] args) {
-        String filePath = "rawData/java-rtf.csv";
-        try {
-            System.setOut(new PrintStream(System.out, true, Charset.defaultCharset()));
-            List<Student> students = CSVToStudents(filePath);
-            for (Student student : students) {
-                System.out.println(student.toString());
-            }
-        } catch (IOException | CsvException e) {
-            System.out.println("Ошибка при чтении CSV: " + e.getMessage());
-        }
-    }
 }
